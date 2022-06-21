479b83222d9d8791b6dcb754a6dffdf1f7b4fcd3
kakhber <beridzekakha@gmail.com>
2022-06-21 17:32:46 +0400

    Primitive types

---
+++ app.js
@@ -0,0 +1,6 @@
+let myName = `Kakha`;
+let myHobby = `Football`;
+const yearOfBirth = 1994;
+let currentYear = 2022;
+let myAge = currentYear - yearOfBirth;
+console.log(`My name is ${myName}, I am ${myAge} old and my hobby is ${myHobby}`);
